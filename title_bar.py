3b4f63dc0e6bbb8c2972957b78be16099df85e9b
Tony Campbell <93364+evilegg@users.noreply.github.com>
2023-11-27 11:59:59 -0500

    Add linear gradient background to text

--- title_bar.py
+++ title_bar.py
@@ -4,6 +4,46 @@ import os
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
+TITLE_COLOR = (255, 244, 102)
+TITLE_SHADOW = BLACK
+SUBTITLE_COLOR = (240, 240, 240)
+SUBTITLE_SHADOW = BLACK
+
+
+def render_textbox(surface, font_path1, font_path2, vertical_offset, title, subtitle):
+    # Set the fonts
+    font1 = pygame.freetype.Font(font_path1, font_size1)
+    font2 = pygame.freetype.Font(font_path2, font_size2)
+
+    # Create text surfaces
+    text1, rect1 = font1.render(title, TITLE_COLOR)
+    text2, rect2 = font2.render(subtitle, SUBTITLE_COLOR)
+
+    # Calculate text positions
+    leading_x = 100
+    line_spacing = 91
+
+    text1_x = leading_x
+    text1_y = vertical_offset + line_spacing * 0.75
+
+    text2_x = leading_x
+    text2_y = text1_y + line_spacing
+
+    font1.render_to(surface, (text1_x + 8, text1_y + 8), title, TITLE_SHADOW, size=font_size1)
+    font1.render_to(surface, (text1_x, text1_y), title, TITLE_COLOR, size=font_size1)
+
+    font2.render_to(surface, (text2_x + 8, text2_y + 8), subtitle, SUBTITLE_SHADOW, size=font_size2)
+    font2.render_to(surface, (text2_x, text2_y), subtitle, SUBTITLE_COLOR, size=font_size2)
+
+
+def render_gradient(surface, y_offset, width, height, start_alpha, end_alpha):
+    delta_alpha = end_alpha - start_alpha
+    for y in range(height):
+        overlay = pygame.Surface((width, 1), pygame.SRCALPHA)
+        overlay.set_alpha(start_alpha + (delta_alpha * y / height))
+        overlay.fill(BLACK)
+        surface.blit(overlay, (0, y_offset + y))
+
 
 def add_text_overlay(input_image_path, output_image_path, line1, line2, font_path1, font_size1, font_path2, font_size2):
     # Initialize Pygame
@@ -16,34 +56,18 @@ def add_text_overlay(input_image_path, output_image_path, line1, line2, font_pat
     # Load the initial image
     image = pygame.image.load(input_image_path).convert_alpha()
 
-    # Set the fonts
-    font1 = pygame.freetype.Font(font_path1, font_size1)
-    font2 = pygame.freetype.Font(font_path2, font_size2)
-
     # Get image dimensions
     image_width, image_height = image.get_size()
 
-    # Create text surfaces
-    text1, rect1 = font1.render(line1, (255, 255, 255))
-    text2, rect2 = font2.render(line2, (255, 255, 255))
-
-
     # Create a surface for the overlay at the bottom with varying opacity
-    overlay_height = 200
-    overlay_surface = pygame.Surface((image_width, overlay_height), pygame.SRCALPHA)
+    overlay_height = 260
+    title_box_y_offset = image_height - overlay_height
 
-    # Calculate text positions
-    text1_x = 0
-    text1_y = image_height - overlay_height
+    gradient_start = 100
+    gradient_end = 255
+    render_gradient(image, title_box_y_offset, image_width, overlay_height, gradient_start, gradient_end)
 
-    text2_x = 0
-    text2_y = image_height - overlay_height + 80
-
-    font1.render_to(image, (text1_x + 16, text1_y + 16), line1, BLACK, size=font_size1)
-    font1.render_to(image, (text1_x, text1_y), line1, WHITE, size=font_size1)
-
-    font2.render_to(image, (text2_x + 8, text2_y + 8), line2, BLACK, size=font_size2)
-    font2.render_to(image, (text2_x, text2_y), line2, WHITE, size=font_size2)
+    render_textbox(image, font_path1, font_path2, title_box_y_offset, line1, line2)
 
     # Save the image copy as the output image
     pygame.image.save(image, output_image_path)
@@ -57,9 +81,9 @@ if __name__ == "__main__":
     line1 = "First line of text"
     line2 = "Second line of text"
     font_path1 = "path/to/your/font1.otf"  # Replace with the path to your first font file
-    font_size1 = 90
+    font_size1 = 91
     font_path2 = "path/to/your/font2.otf"  # Replace with the path to your second font file
-    font_size2 = 48
+    font_size2 = 45
 
     add_text_overlay(input_image_path, output_image_path, line1, line2, font_path1, font_size1, font_path2, font_size2)
 

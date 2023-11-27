45d56f3515a7dfd781f30f868108c17bc41e6c54
Tony Campbell <93364+evilegg@users.noreply.github.com>
2023-11-27 10:29:49 -0500

    Add a dropshadow to the text

--- title_bar.py
+++ title_bar.py
@@ -1,8 +1,9 @@
-#!/usr/bin/env python3
-"""Add a consistent title bar to a thumbnail.
-"""
 import pygame
 import os
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
 
 def add_text_overlay(input_image_path, output_image_path, line1, line2, font_path1, font_size1, font_path2, font_size2):
     # Initialize Pygame
@@ -19,46 +20,33 @@ def add_text_overlay(input_image_path, output_image_path, line1, line2, font_pat
     font1 = pygame.freetype.Font(font_path1, font_size1)
     font2 = pygame.freetype.Font(font_path2, font_size2)
 
+    # Get image dimensions
+    image_width, image_height = image.get_size()
+
     # Create text surfaces
     text1, rect1 = font1.render(line1, (255, 255, 255))
     text2, rect2 = font2.render(line2, (255, 255, 255))
 
-    # Get image dimensions
-    image_width, image_height = image.get_size()
-
-    # Calculate text positions
-    text1_x = (image_width - rect1.width) // 2
-    text1_y = image_height - 60
-
-    text2_x = (image_width - rect2.width) // 2
-    text2_y = image_height - 30
 
     # Create a surface for the overlay at the bottom with varying opacity
-    overlay_height = 260
+    overlay_height = 200
     overlay_surface = pygame.Surface((image_width, overlay_height), pygame.SRCALPHA)
 
-    for alpha in range(128, 256, 2):  # Opacity from 50% to 100% (128 to 255)
-        overlay_surface.set_alpha(alpha)
-        overlay_surface.fill((0, 0, 0, alpha))
+    # Calculate text positions
+    text1_x = 0
+    text1_y = image_height - overlay_height
 
-        # Create a copy of the initial image to overlay on
-        image_copy = image.copy()
+    text2_x = 0
+    text2_y = image_height - overlay_height + 80
 
-        # Blit the overlay onto the image copy
-        image_copy.blit(overlay_surface, (0, image_height - overlay_height))
+    font1.render_to(image, (text1_x + 16, text1_y + 16), line1, BLACK, size=font_size1)
+    font1.render_to(image, (text1_x, text1_y), line1, WHITE, size=font_size1)
 
-        # Blit the original image onto the image copy
-        image_copy.blit(image, (0, 0))
+    font2.render_to(image, (text2_x + 8, text2_y + 8), line2, BLACK, size=font_size2)
+    font2.render_to(image, (text2_x, text2_y), line2, WHITE, size=font_size2)
 
-        # Blit the original first line and the second line onto the image copy
-        image_copy.blit(text1, (text1_x, text1_y))
-        image_copy.blit(text2, (text2_x, text2_y))
-
-        # Save the image copy as the output image
-        pygame.image.save(image_copy, output_image_path)
-
-        # Delay to show the image for a short duration (adjust as needed)
-        pygame.time.delay(30)  # 30 milliseconds
+    # Save the image copy as the output image
+    pygame.image.save(image, output_image_path)
 
     # Quit Pygame
     pygame.quit()
@@ -69,10 +57,9 @@ if __name__ == "__main__":
     line1 = "First line of text"
     line2 = "Second line of text"
     font_path1 = "path/to/your/font1.otf"  # Replace with the path to your first font file
-    font_size1 = 36
+    font_size1 = 90
     font_path2 = "path/to/your/font2.otf"  # Replace with the path to your second font file
-    font_size2 = 24
+    font_size2 = 48
 
     add_text_overlay(input_image_path, output_image_path, line1, line2, font_path1, font_size1, font_path2, font_size2)
-
 

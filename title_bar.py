aecf74849c008083595eab9a5beeb63f29e2ef2f
Tony Campbell <93364+evilegg@users.noreply.github.com>
2023-11-27 12:42:38 -0500

    Add click argument processing

--- title_bar.py
+++ title_bar.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 """Draw a title box on a cover image for my channel"""
 
+import base64
+from io import StringIO
 import os
 
+import click
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '0'
+import fonts
 import pygame
+
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
@@ -15,10 +20,10 @@ SUBTITLE_COLOR = (240, 240, 240)
 SUBTITLE_SHADOW = BLACK
 
 
-def render_textbox(surface, font_path1, font_path2, vertical_offset, title, subtitle):
+def render_textbox(surface, title_font, subtitle_font, vertical_offset, title, title_font_size, subtitle, subtitle_font_size):
     # Set the fonts
-    font1 = pygame.freetype.Font(font_path1, font_size1)
-    font2 = pygame.freetype.Font(font_path2, font_size2)
+    font1 = pygame.freetype.Font(title_font or fonts.MINECRAFT_BOLD, title_font_size)
+    font2 = pygame.freetype.Font(subtitle_font or fonts.MINECRAFT_ITALIC, subtitle_font_size)
 
     # Create text surfaces
     text1, rect1 = font1.render(title, TITLE_COLOR)
@@ -34,23 +39,52 @@ def render_textbox(surface, font_path1, font_path2, vertical_offset, title, subt
     text2_x = leading_x
     text2_y = text1_y + line_spacing
 
-    font1.render_to(surface, (text1_x + 8, text1_y + 8), title, TITLE_SHADOW, size=font_size1)
-    font1.render_to(surface, (text1_x, text1_y), title, TITLE_COLOR, size=font_size1)
+    font1.render_to(surface, (text1_x + 8, text1_y + 8), title, TITLE_SHADOW, size=title_font_size)
+    font1.render_to(surface, (text1_x, text1_y), title, TITLE_COLOR, size=title_font_size)
 
-    font2.render_to(surface, (text2_x + 8, text2_y + 8), subtitle, SUBTITLE_SHADOW, size=font_size2)
-    font2.render_to(surface, (text2_x, text2_y), subtitle, SUBTITLE_COLOR, size=font_size2)
+    font2.render_to(surface, (text2_x + 8, text2_y + 8), subtitle, SUBTITLE_SHADOW, size=subtitle_font_size)
+    font2.render_to(surface, (text2_x, text2_y), subtitle, SUBTITLE_COLOR, size=subtitle_font_size)
 
 
 def render_gradient(surface, y_offset, width, height, start_alpha, end_alpha):
+    """Behind the text is a dark radiant box."""
     delta_alpha = end_alpha - start_alpha
     for y in range(height):
         overlay = pygame.Surface((width, 1), pygame.SRCALPHA)
         overlay.set_alpha(start_alpha + (delta_alpha * y / height))
         overlay.fill(BLACK)
         surface.blit(overlay, (0, y_offset + y))
+        
 
-
-def add_text_overlay(input_image_path, output_image_path, line1, line2, font_path1, font_size1, font_path2, font_size2):
+@click.command()
+@click.option('--input-image', '-i',
+              type=click.Path(exists=True),
+              default="path/to/your/image.jpg",
+              help='Path to the input image file.')
+@click.option('--output-image', '-o',
+              type=click.Path(),
+              help='Path to the output image file.')
+@click.option('--title', '-t',
+              default='This episode',
+              help='The title of this episode.')
+@click.option('--subtitle', '-s',
+              default='',
+              help='Useful sub-title')
+@click.option('--title-font',
+              type=click.File('rb'),
+              help='Path to an optional font file to use for the title.')
+@click.option('--subtitle-font',
+              type=click.File('rb'),
+              help='Path to an optional font file to use for the subtitle.')
+@click.option('--title-font-size',
+              type=int,
+              default=90,
+              help='Font size for the title.')
+@click.option('--subtitle_font_size',
+              type=int,
+              default=45,
+              help='Font size for the subtitle.')
+def main(input_image, output_image, title, subtitle, title_font, title_font_size, subtitle_font, subtitle_font_size):
     # Initialize Pygame
     pygame.init()
 
@@ -59,7 +93,7 @@ def add_text_overlay(input_image_path, output_image_path, line1, line2, font_pat
     pygame.display.set_mode(dummy_size)
 
     # Load the initial image
-    image = pygame.image.load(input_image_path).convert_alpha()
+    image = pygame.image.load(input_image).convert_alpha()
 
     # Get image dimensions
     image_width, image_height = image.get_size()
@@ -72,23 +106,20 @@ def add_text_overlay(input_image_path, output_image_path, line1, line2, font_pat
     gradient_end = 255
     render_gradient(image, title_box_y_offset, image_width, overlay_height, gradient_start, gradient_end)
 
-    render_textbox(image, font_path1, font_path2, title_box_y_offset, line1, line2)
+    render_textbox(image, title_font, subtitle_font, title_box_y_offset, title, title_font_size, subtitle, subtitle_font_size)
 
     # Save the image copy as the output image
-    pygame.image.save(image, output_image_path)
+    if not output_image:
+        print(output_image)
+        path_fragments = os.path.splitext(input_image)
+        output_image = ''.join((path_fragments[0], '-output', path_fragments[1]))
+
+    pygame.image.save(image, output_image)
 
     # Quit Pygame
     pygame.quit()
 
-if __name__ == "__main__":
-    input_image_path = "path/to/your/image.jpg"
-    output_image_path = "path/to/your/output_image.jpg"
-    line1 = "First line of text"
-    line2 = "Second line of text"
-    font_path1 = "path/to/your/font1.otf"  # Replace with the path to your first font file
-    font_size1 = 91
-    font_path2 = "path/to/your/font2.otf"  # Replace with the path to your second font file
-    font_size2 = 45
 
-    add_text_overlay(input_image_path, output_image_path, line1, line2, font_path1, font_size1, font_path2, font_size2)
+if __name__ == "__main__":
+    main()
 
